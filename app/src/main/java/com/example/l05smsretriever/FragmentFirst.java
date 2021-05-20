2356667b2f76f37a3d34263d6575cf9f427adbd1
LI SHUFANG <lishufang1121@gmail.com>
2021-05-20 15:02:47 +0800

    Merge branch 'master' of https://github.com/Myron819/L05_SMS_Retriever
     Conflicts:
    	app/src/main/java/com/example/l05smsretriever/FragmentFirst.java

--- app/src/main/java/com/example/l05smsretriever/FragmentFirst.java
+++ app/src/main/java/com/example/l05smsretriever/FragmentFirst.java
@@ -3,80 +3,58 @@ package com.example.l05smsretriever;
 import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.format.DateFormat;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.PermissionChecker;
 import androidx.fragment.app.Fragment;
 
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
 public class FragmentFirst extends Fragment {
+    Button btnRetrieveNum;
     EditText etNum;
-    TextView tvResult;
-    Button btnRetrieveSMS, btnSendEmail;
+    TextView tvNumResult;
 
     public FragmentFirst() {
         // Required empty public constructor
     }
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_first);
-
-        // Binding UI variables
-        etNum = findViewById(R.id.etNum);
-        tvResult = findViewById(R.id.tvNumResult);
-        // btnSendEmail = findViewById(R.id.btnSendEmail);
-        btnRetrieveSMS = findViewById(R.id.btnRetrieveNum);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        etNum = view.findViewById(R.id.etNum);
+        btnRetrieveNum = view.findViewById(R.id.btnRetrieveNum);
+        tvNumResult = view.findViewById(R.id.tvNumResult);
 
 
-        btnSendEmail.setOnClickListener(v -> {
-
-            Intent emailIntent = new Intent(Intent.ACTION_SEND);
-
-            emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"19039480@rp.edu.sg"});
-            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Testing Email Intent");
-            String statement = tvResult.getText().toString();
-            emailIntent.putExtra(Intent.EXTRA_TEXT, statement);
-
-            emailIntent.setType("message/rfc822");
-
-            startActivity(Intent.createChooser(emailIntent, "Choose an Email client :"));
-
-
-        });
-        btnRetrieveSMS.setOnClickListener(view -> {
-
-            // todo: 9.	To include the runtime check in the app
-            int permissionCheck = PermissionChecker.checkSelfPermission(FragmentFirst.this, Manifest.permission.READ_SMS);
+        btnRetrieveNum.setOnClickListener(view1 -> {
+            int permissionCheck = PermissionChecker.checkSelfPermission(getActivity(), Manifest.permission.READ_SMS);
 
             if (permissionCheck != PermissionChecker.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(FragmentFirst.this, new String[]{Manifest.permission.READ_SMS}, 0);
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_SMS}, 0);
                 return;
             }
-
-            // Create all messages URI
             Uri uri = Uri.parse("content://sms");
 
-            // The columns we want
-            //  date is when the message took place
-            //  address is the number of the other party
-            //  body is the message content
-            //  type 1 is received, type 2 sent
-            String[] reqCols = new String[]{"numb", "address", "body", "type"};
+            String[] reqCols = new String[]{"date", "address", "body", "type"};
 
-            // Get Content Resolver object from which to
-            //  query the content provider
-            ContentResolver cr = getContentResolver();
+            ContentResolver cr = getActivity().getContentResolver();
 
 
             // Todo: 3.	When user enters a number under Only SMS containing this number,
@@ -88,7 +66,6 @@ public class FragmentFirst extends Fragment {
 
             Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
             String smsBody = "";
-
             if (cursor.moveToFirst()) {
                 do {
                     long dateInMillies = cursor.getLong(0);
@@ -104,43 +81,9 @@ public class FragmentFirst extends Fragment {
                     smsBody += type + " " + address + "\n at" + date + "\n\"" + body + "\"\n\n";
                 } while (cursor.moveToNext());
             }
-            tvResult.setText(smsBody);
+            tvNumResult.setText(smsBody);
         });
 
-    }
-
-    // Todo: 10.	The user will have the option to grant the permission.
-    //  In any case, you can choose to react with actions after the user makes his choice on granting the permission.
-   /* onRequestPermissionsResult()  will be triggered (it’s a callback method)
-   after the request for permission dialog is shown to the user.
-
-   You can choose the actions to be associated with the cases where:
-    i)	Permission is granted
-    Mostly like, you’ll want to continue where it was left off when the dialog was triggered.
-    In this case, the button click event to be triggered.
-
-    ii)	Permission is denied
-    In this case, a feedback to the user is necessary and a Toast will be appropriate.
-    */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 0: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the read SMS
-                    //  as if the btnRetrieve is clicked
-                    btnRetrieveSMS.performClick();
-
-                } else {
-                    // permission denied... notify user
-                    Toast.makeText(FragmentFirst.this, "Permission not granted",
-                            Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        }
+        return view;
     }
 }
--- app/src/main/java/com/example/l05smsretriever/FragmentSecond.java
+++ app/src/main/java/com/example/l05smsretriever/FragmentSecond.java
@@ -2,76 +2,76 @@ package com.example.l05smsretriever;
 
 import android.Manifest;
 import android.content.ContentResolver;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.format.DateFormat;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.PermissionChecker;
 import androidx.fragment.app.Fragment;
 
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
 public class FragmentSecond extends Fragment {
+    Button btnRetrieveWord;
     EditText etWord;
     TextView tvWordResult;
-    Button btnRetrieveWord;
 
     public FragmentSecond() {
         // Required empty public constructor
     }
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_second);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
 
-        // Binding UI variables
-        etWord = findViewById(R.id.etWord);
-        tvWordResult = findViewById(R.id.tvWordResult);
-        btnRetrieveWord = findViewById(R.id.btnRetrieveWord);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        etWord = view.findViewById(R.id.etWord);
+        btnRetrieveWord = view.findViewById(R.id.btnRetrieveWord);
+        tvWordResult = view.findViewById(R.id.tvWordResult);
 
-
-        btnRetrieveWord.setOnClickListener(view -> {
-
-            // todo: 9.	To include the runtime check in the app
-            int permissionCheck = PermissionChecker.checkSelfPermission(FragmentSecond.this, Manifest.permission.READ_SMS);
+        btnRetrieveWord.setOnClickListener(view1 -> {
+            int permissionCheck = PermissionChecker.checkSelfPermission(getActivity(), Manifest.permission.READ_SMS);
 
             if (permissionCheck != PermissionChecker.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(FragmentSecond.this, new String[]{Manifest.permission.READ_SMS}, 0);
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_SMS}, 0);
                 return;
             }
-
-            // Create all messages URI
             Uri uri = Uri.parse("content://sms");
 
-            // The columns we want
-            //  date is when the message took place
-            //  address is the word of the other party
-            //  body is the message content
-            //  type 1 is received, type 2 sent
-            String[] reqCols = new String[]{"numb", "address", "body", "type"};
+            String[] reqCols = new String[]{"date", "address", "body", "type"};
 
-            // Get Content Resolver object from which to
-            //  query the content provider
-            ContentResolver cr = getContentResolver();
+            ContentResolver cr = getActivity().getContentResolver();
 
 
-            // TODO: 4.	When user enters a word under Only SMS with this word,
+            // TODO: 4.	When user enters a word under Only SMS with this word, 
             //  it will retrieve only SMS that contains the entered keyword
-            String filter = "address LIKE ?";
+            String filter = "body LIKE ?";
             String word = etWord.getText().toString();
-            String[] filterArgs = {"%" + word + "%"};
+            String[] splitedWord = word.split(" ");
+            String[] filterArgs = new String[splitedWord.length];
 
+            for (int i = 0; i < splitedWord.length; i++) {
+                if (splitedWord.length != 1) {
+                    filter += " OR body LIKE ?";
+                }
+                filterArgs[i] = "%" + splitedWord[i] + "%";
+            }
 
             Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
             String smsBody = "";
-
             if (cursor.moveToFirst()) {
                 do {
                     long dateInMillies = cursor.getLong(0);
@@ -89,41 +89,6 @@ public class FragmentSecond extends Fragment {
             }
             tvWordResult.setText(smsBody);
         });
-
-    }
-
-    // Todo: 10.	The user will have the option to grant the permission.
-    //  In any case, you can choose to react with actions after the user makes his choice on granting the permission.
-   /* onRequestPermissionsResult()  will be triggered (it’s a callback method)
-   after the request for permission dialog is shown to the user.
-
-   You can choose the actions to be associated with the cases where:
-    i)	Permission is granted
-    Mostly like, you’ll want to continue where it was left off when the dialog was triggered.
-    In this case, the button click event to be triggered.
-
-    ii)	Permission is denied
-    In this case, a feedback to the user is necessary and a Toast will be appropriate.
-    */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 0: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the read SMS
-                    //  as if the btnRetrieve is clicked
-                    btnRetrieveWord.performClick();
-
-                } else {
-                    // permission denied... notify user
-                    Toast.makeText(FragmentSecond.this, "Permission not granted",
-                            Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        }
+        return view;
     }
 }
